6f413d6a9b7fb1226f6bec68e64bb57bbdfcce8f
vandana singandipe <vandana.singandipe@gmail.com>
2021-01-23 21:49:16 +0530

    extracted all heading by css selector

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 /*code for printing Publication Date heading */
 console.clear();
 const publicationDateHeading= document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(1) > span > font > font");
+
 console.log(publicationDateHeading.innerText);
 
 /*code for printing Publication Date   */
 const publicationDate=document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(1) > font> font");
+
 console.log(publicationDate.innerText);
 
 /*code for printing Bidding Heading Date heading */
@@ -13,19 +15,74 @@ const biddingDateHeading =document.querySelector("div.views-field.views-field-no
 
 console.log(biddingDateHeading.innerText);
 
-/*code for printing Bidding Date heading */
+/*code for printing Bidding Date  */
 const biddingDate=document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(19) > font > font")
+
 console.log(biddingDate.innerText);
 
-/*code for printing object Heading heading */
+/*code for printing object  heading */
 
 const objectHeading =document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(4) > span > font > font");
+
 console.log(objectHeading.innerText);
 
-/*code for printing object text heading */
+/*code for printing object text */
 
 const objectText =document.querySelector("div.views-field.views-field-nothing > span > p:nth-child(6) > font > font");
+
 console.log(objectText.innerText);
+
+/*code for printing Addition information  heading */
+
+const additionInfo=document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(8) > span > font > font");
+
+console.log(additionInfo.innerText);
+
+/*code for printing Addition information text */
+
+const fileNumber=document.querySelector("div.views-field.views-field-nothing > span > p:nth-child(10) > font > font");
+
+console.log(fileNumber.innerText);
+
+const bankName=document.querySelector("div.views-field.views-field-nothing > span > p:nth-child(11) > font > font");
+
+console.log(bankName.innerText);
+
+/*code for printing modality heading */
+const modalityHeading=document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(13) > span > font > font");
+
+console.log(modalityHeading.innerText);
+
+/*code for printing modality text */
+
+const modalityText =document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(13) > font > font");
+
+console.log(modalityText.innerText);
+
+/*code for printing situation heading */
+const situationHeading=document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(16) > span > font > font");
+
+console.log(situationHeading.innerText);
+
+/*code for printing situation text */
+const situationText=document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(16) > font > font");
+
+console.log(situationText.innerText);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
