3f26419afc7b95523eff3ee18d0c356e0e76af81
vandana singandipe <vandana.singandipe@gmail.com>
2021-01-23 20:29:25 +0530

    object data extracted

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*code for printing Publication Date heading */
+console.clear();
+const publicationDateHeading= document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(1) > span > font > font");
+console.log(publicationDateHeading.innerText);
+
+/*code for printing Publication Date   */
+const publicationDate=document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(1) > font> font");
+console.log(publicationDate.innerText);
+
+/*code for printing Bidding Heading Date heading */
+
+const biddingDateHeading =document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(19) > span > font > font");
+
+console.log(biddingDateHeading.innerText);
+
+/*code for printing Bidding Date heading */
+const biddingDate=document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(19) > font > font")
+console.log(biddingDate.innerText);
+
+/*code for printing object Heading heading */
+
+const objectHeading =document.querySelector("div.views-field.views-field-nothing > span > span:nth-child(4) > span > font > font");
+console.log(objectHeading.innerText);
+
+/*code for printing object text heading */
+
+const objectText =document.querySelector("div.views-field.views-field-nothing > span > p:nth-child(6) > font > font");
+console.log(objectText.innerText);
+
+
+
+
+
+
